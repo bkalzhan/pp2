a0476ed391c79694e86ce6aaa21c3cf318fdfffc
Bekzat Kalzhan <kalzhanbekzat99@gmail.com>
2019-02-10 14:19:13 +0600

    task2

---
+++ week1/task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Student //создаем класс студент
+    {
+        public string name; // объявляем переменные 
+        public string id;
+        public int year;
+
+        public Student(string name, string id)  // создаем конструктор который принимает 2 переменные имя и айди
+        {
+            this.name = name;
+            this.id = id;
+        }
+        public void plusyear() // функция для добав. года
+        {
+            year++;
+        }
+        public void cout() //функция для вывода перемиенных
+        {
+            Console.WriteLine(name + " " + id + " " + year);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(); //считываем стринг, разделяем на 3 стринга
+            Student newby = new Student(s[0], s[1]);
+            newby.year = int.Parse(s[2]);
+            newby.plusyear();
+            newby.cout();
+            Console.ReadKey();
+        }
+    }
+}
