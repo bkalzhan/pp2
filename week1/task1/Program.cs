0053378a0b9c335ce738a9a4b98d201cc5e11541
Bekzat Kalzhan <kalzhanbekzat99@gmail.com>
2019-02-13 22:32:25 +0600

    sss

--- week1/task1/Program.cs
+++ week1/task1/Program.cs
@@ -10,13 +10,13 @@ namespace task1
     {
         static void Main(string[] args)
         {
-            int cnt = 0; //to find number of prime integers 
-            int n = int.Parse(Console.ReadLine()); //number of input integers 
-            int[] array = new int[n]; //a new array to save prime numbers
-            string[] s = Console.ReadLine().Split(); //the numbers 
+            int cnt = 0; // переменные чтобы считать количество прайм чисел 
+            int n = int.Parse(Console.ReadLine()); //количество инпут цифр
+            int[] array = new int[n]; //новый массив сохранить прайм чисел
+            string[] s = Console.ReadLine().Split(); 
             for (int i = 0; i < n; i++)
             {
-                int cnt1 = 0;//for counting number of possible dividers without remainder
+                int cnt1 = 0;// считать количество делителей
                 int k = int.Parse(s[i]);
                 for (int j = 1; j < k; j++)
                 {
@@ -26,8 +26,8 @@ namespace task1
                 if (cnt1 == 1)
                     array[cnt++] = k;
             }
-            Console.WriteLine(cnt); //the first output- number of the prime integers
-            for (int i = 0; i < cnt; i++) //loop to output each of the prime integers
+            Console.WriteLine(cnt); // колич. прайм чисел
+            for (int i = 0; i < cnt; i++) // прайм числы
                 Console.Write(array[i] + " ");
             Console.ReadKey();
         }
