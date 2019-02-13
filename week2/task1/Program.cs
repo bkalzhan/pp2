c51d3b87e9367ff1223caae7b1e9ca3586dd0cf2
Bekzat Kalzhan <kalzhanbekzat99@gmail.com>
2019-02-13 23:34:25 +0600

    1111

--- week2/task1/Program.cs
+++ week2/task1/Program.cs
@@ -9,27 +9,21 @@ namespace task1
 {
     class Program
     {
-        public static void  Fox()                //создаем функцию 
+        static void Main(string[] args)
         {
             StreamReader sr = new StreamReader(@"C:\pp2\week2\task1\input.txt");      //считываем стринг через инпут файл
-            string s = sr.ReadToEnd();
-            string[] a = s.Split();
-            for (int i = 0; i < a.Length / 2; i++)     //проверяем стринг на палиндром и выводим ответ
+            string[] s = sr.ReadLine().Split();
+            for (int i = 0; i < s.Length / 2; i++)     //проверяем стринг на палиндром и выводим ответ
             {
-                if (a[i] != a[a.Length - i - 1])
+                if (s[i] != s[s.Length - i - 1])
                 {
                     Console.WriteLine("No");
                     return;
                 }
-
             }
             sr.Close();
             Console.WriteLine("Yes");
-
-        }
-        static void Main(string[] args)
-        {
-            Fox();// вызываем функцию
+            Console.ReadKey();
         }
     }
 }
